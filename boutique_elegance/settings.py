fbfd9870c0707fb3ffad16d30101e96d3bed7378
Erma Zunic <zunicerma@gmail.com>
2024-07-13 15:10:16 +0000

    stripe part 7

--- boutique_elegance/settings.py
+++ boutique_elegance/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,6 +186,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+print("public key = ",STRIPE_PUBLIC_KEY)
+print("secret key = ",STRIPE_SECRET_KEY)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
