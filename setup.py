0faaebd8fb063c3d3acee6987ed5e660bde321c4
Erma Zunic <zunicerma@gmail.com>
2024-07-19 17:45:41 +0000

    deployment try

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+
+from setuptools import find_packages, setup  # Remove convert_path
+
+# Define a dummy convert_path function if necessary
+def convert_path(path):
+    return path
+
+# Continue with the rest of the setup script
