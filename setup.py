76fa4f4621a996cfd4d5c6c09204601b46cd542c
Erma Zunic <zunicerma@gmail.com>
2024-07-19 18:45:37 +0000

    edditing

--- setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-
-from setuptools import find_packages, setup  # Remove convert_path
-
-# Define a dummy convert_path function if necessary
-def convert_path(path):
-    return path
-
-# Continue with the rest of the setup script
